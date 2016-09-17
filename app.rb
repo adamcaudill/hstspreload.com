4a4f0f9a3a595e3f6d42aa5d482086f76cac1cd6
Adam Caudill <adam@adamcaudill.com>
2016-09-17 16:41:00 -0400

    Add cache control headers

--- app.rb
+++ app.rb
@@ -4,11 +4,13 @@ require './lookup'
 class HstsPreload < Sinatra::Base
   get '/' do
     #display home page
+    cache_control :public, :max_age => 36000
     erb :index
   end
 
   get '/api/v1/status/:domain' do
     content_type :json
+    cache_control :public, :max_age => 3600
     Lookup.get_status params['domain']
   end
 end
